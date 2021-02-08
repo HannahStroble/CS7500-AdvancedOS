277759fbfb87d8bc8bca8173773ec813e2a0a277
root <root@localhost.localdomain>
2021-02-07 22:24:50 -0500

    pipes working, fixing return number error

--- project2/pwordcount.c
+++ project2/pwordcount.c
@@ -13,36 +13,25 @@
 // main function
 int main(int argc, char *argv[])
 {
-    // main variables
+    // pipe variables
     char write_msg[BUFFER_SIZE];
     char read_msg[BUFFER_SIZE];
+    pid_t pid;
+    int fd1[2]; // pipe 1
+    int fd2[2]; // pipe 2
+    // function variables
     FILE * file;
     int words;
-    pid_t pid;
-    int fd[2];
-
-    // open file
-    file = fopen(argv[1], "r");
     
-    // check if the file has opened successfully
-    if (file == NULL)
-    {
-        err_msg(1);
-        exit(EXIT_FAILURE);
-    }
     
-    int num = count_words(file);
-    printf("%d", num);
-    
-    /*
     // create pipe
-    if (pipe(fd) == -1)
+    if (pipe(fd1) == -1 || pipe(fd2) == -1)
     {
-        fprintf(stderr,"Pipe Failed");
+        fprintf(stderr,"One of the Pipes Failed");
         return 1;
     }
 
-    // fork a child process
+    // fork a process
     pid = fork();
     if (pid < 0)
     {
@@ -52,14 +41,49 @@ int main(int argc, char *argv[])
     // parent process
     if (pid > 0)
     {
-        
+      // close unneeded pipe1 read and pipe2 write
+      close(fd1[READ_END]);
+      close(fd2[WRITE_END]);
+
+      // open file
+      printf("Process 1 is reading file %s now ... \n", argv[0]);
+      file = fopen(argv[1], "r");
+
+      // check if file opened successfully
+      if (file == NULL)
+      {
+        err_msg(1);
+        exit(EXIT_FAILURE);
+      }
+
+      // send file data to child process
+      printf("Process 1 starts sending data to Process 2 ...\n");
+      write(fd1[WRITE_END], file, BUFFER_SIZE);
+      //wait(3);
+      read(fd2[READ_END], read_msg, BUFFER_SIZE);
+      printf("Process 1: the total number of words is %d \n", read_msg);
+      //close(fd1[WRITE_END]);
+      //close(fd2[READ_END]);   
     }
     // child process
     else
     {
-        
+      // close unneeded pipe1 write and pipe2 read
+      close(fd1[WRITE_END]);
+      close(fd2[READ_END]);
+ 
+      //printf("inside child\n");
+      printf("Process 2 finishes receiving data from Process 1...\n");
+      read(fd1[READ_END], file, BUFFER_SIZE);
+      printf("Process 2 is counting words now...\n");
+      //sprintf(write_msg, "%ld", count_words(file));
+      printf("Process 2 is sending the result back to Process 1 ...\n");
+      write(fd2[WRITE_END], "6", BUFFER_SIZE);
+      //printf("child read %s\n", read_msg);
+      //close(fd1[READ_END]);
+      //close(fd2[WRITE_END]);  
     }
-    */
+    fclose(file);   
 
     return 0;
 }
