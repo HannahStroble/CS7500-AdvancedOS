358cdfe36e9d2f62bc3f133189ae4dbd07518733
Hannah Reinbolt <hannah@master-logic.com>
2021-03-21 00:15:04 -0700

    finished microbatch and commenting

--- project3/micro.c
+++ project3/micro.c
@@ -1,47 +1,67 @@
-
-/* 
- * COMP7500/7506
- * Project 3: 
- * ./dispatch 7
- * This will sleep for 7 seconds. Capability for it to take 
- * multiple processes at the same time is setup but not necessary.
- * It should only take one process at a time.
- */
 ///////////////////////////////////////////////////////////////////
 // COMP7500 - Project 3 - A Pthread-based Batch Scheduling System
 // Name: Hannah Reinbolt
 // Date: 3/14/2021
-// Description: Run everything in main, sync main variables, execute threads.
+// Description: Microbatch program that will run for X user defined time.
 // Note: Referenced code provided by Dr. Qin
 //
 // How to run:
-// >> make
-// >> ./aubatch
+// >> gcc -o micro micro.c -lpthread
+// >> ./micro 3
+// This will run for 3 seconds.
 //
 ///////////////////////////////////////////////////////////////////
-#include <stdio.h> 
+
+
+// libraries
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <math.h>
+#include <stdbool.h>
+#include <sys/wait.h>
 
+
+////////////////////////////////////////////////
+// name: main
+// use: runs some process for user defined number of seconds
+// description: run calculations on cpu as a child process counts time for user defined number of seconds
+//
+// input: int argc
+// input description: number of argv
+// input: char *argv[]
+// input description: char array of command line input
+// output: int
+// output description: dummy value
+////////////////////////////////////////////////
 void main(int argc, char *argv[])
 {
-  // use cpu for i amount of time
-  //printf("your number is: %s", argv[1]);
-  int i;
-  while (i < atoi(argv[1]))
-  {
-    int j;
-    for (j=0; j<214000000; j++)
+  // create fork
+    pid_t child, pid;
+    int count = 0;
+    child = fork();
+
+    // start child fork
+    if (child < 0)
     {
-      volatile double x = 9999999.0;
-      volatile double y = cbrt(x);
-      j++;
+        printf("Failed to create a new process.\n");
     }
-    i++;
-  }
-  sleep(atoi(argv[1]));
+    // execute child fork
+    // as child is sleeping, the cpu will be working on the parent in parallel
+    // this will be consistently working the cpu for a user defined amount of time
+    else if (child == 0)
+    {
+        // sleep for user defined amount of time
+        sleep(atoi(argv[1]));
+        exit(0);
+    }
+    // wait for child process to complete before updating finished list
+    while ((pid = wait(&count)) > 0)
+    {
+      // do this cpu calculation for as long as waiting for child
+      double calculation = 1356/123098;
+    }
+
 }
